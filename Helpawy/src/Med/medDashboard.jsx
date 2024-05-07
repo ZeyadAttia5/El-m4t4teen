62446d1598233dea0a099117d366d1f5121dcae3
omar-medhat2 <109584488+omar-medhat2@users.noreply.github.com>
2024-05-07 06:38:10 +0300

    Commit Menu

---
+++ Helpawy/src/Med/medDashboard.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './medDashboard.css'; // Importing the CSS file
+
+function MedDashboard() {
+  return (
+    <div className="dashboard">
+    <h1>Welcome to the Medical Donation Dashboard</h1>
+    <div className="dashboard-options">
+      <div className="dashboard-option medical-supplies">
+        <h2>Medical Supplies</h2>
+        <p>Donate medical supplies such as bandages, gloves, etc.</p>
+      </div>
+      <div className="dashboard-option medication">
+        <h2>Medication</h2>
+        <p>Donate unused medication for those in need.</p>
+      </div>
+      <div className="dashboard-option blood-donation">
+        <h2>Blood Donation</h2>
+        <p>Donate blood to help save lives.</p>
+      </div>
+      <div className="dashboard-option doctor-visit">
+        <h2>Doctor Visit</h2>
+        <p>Volunteer to provide medical consultations.</p>
+      </div>
+    </div>
+  </div>
+  );
+}
+
+export default MedDashboard;
