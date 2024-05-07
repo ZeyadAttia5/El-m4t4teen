7a6d8622b027e38c1b35aac47c9f80f52263b129
ZeyadAttia5 <85710632+ZeyadAttia5@users.noreply.github.com>
2024-05-07 22:58:38 +0300

    fixed import errors

--- Helpawy/src/App.jsx
+++ Helpawy/src/App.jsx
@@ -7,11 +7,6 @@ import LoginRedirect from './RedirectToLogin/LoginRedirect.jsx'
 
 import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
 import AdminPage from './LoginPage/AdminPage';
-import RequestCard from './RequestPost/RequestCard';
-
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import AdminPage from './LoginPage/AdminPage.jsx';
-
 
 // my User stories: 55, 56, 57, 58, 59, 66, 67, 68, 69, 70, 71, 72
 // the ones I finished: 55, 56, 57, 58, 59, 66
