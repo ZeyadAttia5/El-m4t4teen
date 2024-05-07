0ac46c5ebcd521eb3359aacbe72ff26afcb99353
ZeyadAttia5 <85710632+ZeyadAttia5@users.noreply.github.com>
2024-05-07 22:57:56 +0300

    fixed smth

--- Helpawy/src/App.jsx
+++ Helpawy/src/App.jsx
@@ -4,18 +4,18 @@ import SchedulePicker from './SchedulePicker/SchedulePicker.jsx'
 import ScheduleDropoff from './ScheduleDropoff/ScheduleDropoff.jsx'
 import RedirectionButton from './RedirectionButton/RedirectionButton.jsx'
 import LoginRedirect from './RedirectToLogin/LoginRedirect.jsx'
-<<<<<<< HEAD
+
 import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
 import AdminPage from './LoginPage/AdminPage';
 import RequestCard from './RequestPost/RequestCard';
-=======
+
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AdminPage from './LoginPage/AdminPage.jsx';
 
 
 // my User stories: 55, 56, 57, 58, 59, 66, 67, 68, 69, 70, 71, 72
 // the ones I finished: 55, 56, 57, 58, 59, 66
->>>>>>> fd7e677993e442ea727a98011d6892f0aea75e75
+
 
 //comment
 function App() {
