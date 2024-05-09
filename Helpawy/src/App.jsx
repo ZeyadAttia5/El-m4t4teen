993b3e173a44161eb214e29b4ecd5136dd16e594
ZeyadAttia5 <85710632+ZeyadAttia5@users.noreply.github.com>
2024-05-09 20:27:41 +0300

    addded styling to postcard

--- Helpawy/src/App.jsx
+++ Helpawy/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import TransportationPicker from './TransportationPicker/TransportationPicker.jsx'
 import SchedulePicker from './SchedulePicker/SchedulePicker.jsx'
 import ScheduleDropoff from './ScheduleDropoff/ScheduleDropoff.jsx'
-import RedirectionButton from './RedirectionButton/RedirectionButton.jsx'
+
 import LoginRedirect from './RedirectToLogin/LoginRedirect.jsx'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AdminPage from './LoginPage/AdminPage.jsx';
