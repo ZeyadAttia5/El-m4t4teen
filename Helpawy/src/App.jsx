1286378326f06b63c18053aad68028804dc63aa4
amrkhaled04 <105929016+amrkhaled04@users.noreply.github.com>
2024-05-11 11:38:49 +0300

    adding google api

--- Helpawy/src/App.jsx
+++ Helpawy/src/App.jsx
@@ -1,14 +1,7 @@
 import './App.css'
-import TransportationPicker from './TransportationPicker/TransportationPicker.jsx'
-import SchedulePicker from './SchedulePicker/SchedulePicker.jsx'
-import ScheduleDropoff from './ScheduleDropoff/ScheduleDropoff.jsx'
 // import ChooseItemTypePage from './ChooseItemTypePage/ChooseItemTypePage.jsx'; // Assuming you have a component for choosing item type
-import RedirectionButton from './RedirectionButton/RedirectionButton.jsx'
-import { BrowserRouter as Router } from 'react-router-dom';
+import React, { useState } from 'react'
 import 'react-toastify/dist/ReactToastify.css'; // Import toast styles
-import BloodDonationComponent from './Med/BloodDonation.jsx'
-import MedDashboard from './Med/medDashboard.jsx'
-import React, { useState } from 'react';
 import MedicalSuppliesComponent from './Med/medicalSupplies.jsx'
 function App() {
 
