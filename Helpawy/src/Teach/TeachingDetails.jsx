69ae4c1b50c969c1579478761693f502c27569a7
YoussefMansoor <135554908+YoussefMansoor@users.noreply.github.com>
2024-05-12 20:05:18 +0300

    google maps marker

--- Helpawy/src/Teach/TeachingDetails.jsx
+++ Helpawy/src/Teach/TeachingDetails.jsx
@@ -1,16 +1,18 @@
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { GoogleMap, Marker, LoadScript } from '@react-google-maps/api';
+import { GoogleMap,Marker, LoadScript } from '@react-google-maps/api';
 import './TeacherDetails.css';
 
 const teachers = [
   { id: 1, name: 'Math Teacher', subject: 'Math', teachingAreas: 'Madinaty', governorate: 'Cairo', numberOfStudents: 30, address: 'B12', coordinates: { lat: 30.08498749136197, lng: 31.65796306721924 } },
-  { id: 2, name: 'English Teacher', subject: 'English', teachingAreas: 'Sheikh Zayed', governorate: 'Giza', numberOfStudents: 25, address: 'Palm Hills', coordinates: { lat: 30.017, lng: 31.213 } },
-  { id: 3, name: 'Physics Teacher', subject: 'Physics', teachingAreas: 'Sidi Bishr', governorate: 'Alexandria', numberOfStudents: 40, address: 'Mohamad Nageeb St.', coordinates: { lat: 31.2001, lng: 29.9187 } },
+  { id: 2, name: 'English Teacher', subject: 'English', teachingAreas: 'Sheikh Zayed', governorate: 'Giza', numberOfStudents: 25, address: 'Palm Hills', coordinates: { lat: 30.0202228, lng: 31.0032952 } },
+  { id: 3, name: 'Physics Teacher', subject: 'Physics', teachingAreas: 'Sidi Bishr', governorate: 'Alexandria', numberOfStudents: 40, address: 'Mohamad Nageeb St.', coordinates: { lat: 31.2525588, lng: 29.9766823 } },
 ];
+
 
 const TeacherDetails = () => {
   const { id } = useParams();
+  const [mapLoaded, setMapLoaded] = useState(false);
   const teacher = teachers.find(teacher => teacher.id === parseInt(id));
   const [processing, setProcessing] = useState(false);
   const [accepted, setAccepted] = useState(false);
@@ -27,6 +29,10 @@ const TeacherDetails = () => {
     height: "400px",
     width: "100%"
   };
+  const handleMapLoad = () => {
+   
+    setMapLoaded(true);
+  };
 
   return (
     <div className="teacher-details-container">
@@ -39,13 +45,14 @@ const TeacherDetails = () => {
 
       
 
-      <LoadScript googleMapsApiKey="YOUR_API_KEY">
+      <LoadScript googleMapsApiKey="AIzaSyAdzaGmL_O_WXhUqyDe-EPm9qp6f2iHrek">
         <GoogleMap
           mapContainerStyle={mapStyles}
           zoom={14}
           center={teacher.coordinates}
+          onLoad={handleMapLoad}
         >
-          <Marker position={teacher.coordinates} />
+         {mapLoaded && <Marker position={teacher.coordinates} />}
         </GoogleMap>
       </LoadScript>
 
